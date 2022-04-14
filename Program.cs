b6be2f70ce2accfda4a7e8519c6aa2995dfc2b46
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-14 20:11:36 +0530

    [Manoj]Adding_HappyOrSadMood

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome To Mood Analyser Program");
+            MoodAnalyser moodAnalyser = new MoodAnalyser("Happy");
+            Console.WriteLine(moodAnalyser.Mood()); 
+            Console.ReadLine();
+
+        }
+    }
+}
+
