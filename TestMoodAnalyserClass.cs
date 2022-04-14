86734ffb60324c7142a7b16372eb4c221eec39a9
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-14 21:09:42 +0530

    [Manoj]Adding_ReturnSad

--- TestMoodAnalyserClass.cs
+++ TestMoodAnalyserClass.cs
@@ -5,22 +5,38 @@ using MoodAnalyserDemo;
 namespace MsTestMethodAnalyserProject
 {
     [TestClass]
-        public class TestMoodAnalyserClass
+    public class TestMoodAnalyserClass
+    {
+        //[TestMethod]
+        //public void GivenMessageReturnMood()
+        //{
+        //    //Arrange
+        //    string message = "I am in Sad mood";
+        //    string expected = "Sad";
+        //    MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+
+        //    //Act
+        //    string actual = moodAnalyser.Mood();
+        //    //Assert
+        //    Assert.AreEqual(expected, actual);
+
+        //}
+        [TestMethod]
+        public void Given_Happymood_Expecting_Happy_Result()//Method
         {
-            [TestMethod]
-            public void GivenMessageReturnMood()
-            {
-                //Arrange
-                string message = "I am in Sad mood";
-                string expected = "Sad";
-                MoodAnalyser moodAnalyser = new MoodAnalyser(message);
+            string message = "I am in Happy mood";
+            string expected = "Happy";
+            MoodAnalyser moodAnalyser = new MoodAnalyser(message);
 
-                //Act
-                string actual = moodAnalyser.Mood();
-                //Assert
-                Assert.AreEqual(expected, actual);
+            //Act
+            string actual = moodAnalyser.Mood();
+            //Assert
+            Assert.AreEqual(expected, actual);
 
-            }
 
         }
+
+
+
     }
+}
