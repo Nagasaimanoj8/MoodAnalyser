93f1b5f185b07ae065dcd0df2b04a98a784db44a
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-15 17:17:11 +0530

    [Manoj]Adding_Respond

---
+++ MoodAnalyser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyserDemo
+{
+    public class MoodAnalyser
+    {
+        public string msg;
+        public MoodAnalyser(string msg)
+        {
+            this.msg = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            if (msg.ToLower().Contains("sad"))
+                return "SAD";
+
+            else
+                return "HAPPY";
+        }
+    }
+}
