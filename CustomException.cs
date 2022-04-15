f322d6a7d4498b739c47e62a7a0fa204943e6e09
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-15 17:43:26 +0530

    [Manoj]Adding_CustomException

--- CustomException.cs
+++ CustomException.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace MoodAnalyserDemo
 {
-    internal class CustomException
-    {
+      public class CustomException:Exception
+        {
+            public ExceptionType type;
+
+            public enum ExceptionType
+            {
+                Null_Type_Exception,
+                Empty_Type_Exception
+            }
+            public CustomException(ExceptionType type, string message) : base(message)
+            {
+                this.type = type;
+            }
+        }
     }
-}
+
