1ccd36624f5014cb544b9b8c022a5811be30283e
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-04-16 05:25:20 +0530

    [Manoj]Adding_ReturnSad

---
+++ TestMoodAnalyserClass.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using MoodAnalyserDemo;
+
+namespace MsTestMethodAnalyserProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void MsgSadMood()
+        {
+            //Arrange
+            string msg = "I am in sad mood";
+            string expected = "SAD";
+
+            MoodAnalyser mood = new MoodAnalyser(msg);
+            //Act
+            string actual = mood.AnalyseMood();
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        
+
+
+    }
+}
+    
